55979072eba590357f4367767aca1a7552b36146
christianrolph <55676161+christianrolph@users.noreply.github.com>
2021-12-04 12:59:15 -0800

    adds catapult control

---
+++ Assets/CatapultControll.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatapultControll : MonoBehaviour
+{
+    public Vector2 MovementVector;
+    public float ShotPowerAngle;
+    public float AimAxisAngle;
+    public bool isFiring;
+
+    public float MaxPulledBackCatapultArm = -125;
+    public float MinPulledBackCatapultArm = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetMovementVector(Vector2 vectorToSet)
+    {
+        this.MovementVector = vectorToSet;
+    }
+
+    public void SetShotPowerAngle(float angle)
+    {
+        this.ShotPowerAngle = angle;
+    }
+
+    public void SetAimAxisAngle(float angle)
+    {
+        this.AimAxisAngle = angle;
+    }
+
+    public bool Fire()
+    {
+        if (isFiring)
+        {
+            return false;
+        }
+        else
+        {
+            // do some fire stuff
+            return true;
+        }
+    }
+}
