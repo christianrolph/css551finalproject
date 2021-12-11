e3aba5660221715c3a55f0eb36e5d870350393a1
lumanson <camber1camber1@gmail.com>
2021-12-11 10:47:41 -0800

    Gem Destruction sound
    Added sound on gem destruction
    Added score counter to remote
    Moved and enlarged the small camera screen
    added object manipulators on posters
    spawn 3 gems at start

--- Assets/Scripts/GemHandler.cs
+++ Assets/Scripts/GemHandler.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GemHandler : MonoBehaviour
 {
     public GameObject GemPrefab;
     public int GemsDestroyedCount = 0;
+    public TMP_Text ScoreEcho;
+    AudioSource audioSource;
 
     List<GemCollider> gems = new List<GemCollider>();
 
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Assert(GemPrefab != null);   
+        Debug.Assert(GemPrefab != null);
+        Debug.Assert(ScoreEcho != null);
+        audioSource = GetComponent<AudioSource>();
+        Debug.Assert(GetComponent<AudioSource>() != null);
     }
 
     public void CreateGem()
@@ -20,11 +26,18 @@ public class GemHandler : MonoBehaviour
         GameObject gem = Instantiate(GemPrefab, new Vector3(0, 0.2f, -0.2f), Quaternion.identity);
         gems.Add(gem.GetComponent<GemCollider>());
     }
+    public void CreateGem(Vector3 pos)
+    {
+        GameObject gem = Instantiate(GemPrefab, pos, Quaternion.identity);
+        gems.Add(gem.GetComponent<GemCollider>());
+    }
 
     public void GemHit(GemCollider gem, ProjectileBehavior projectile)
     {
+        audioSource.Play();
         gems.Remove(gem);
         GemsDestroyedCount++;
         projectile.DestroyProjectile();
+        ScoreEcho.text = "Score:\n" + GemsDestroyedCount;
     }
 }
